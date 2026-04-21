e7e7a823ca74c0e9dd8a319f539b7ef496f37c21
bridgetukpoju01-droid <bridgetukpoju01@gmail.com>
2026-04-21 16:44:41 +0100

    Add files via upload

---
+++ convertN.go
@@ -0,0 +1,30 @@
+package main
+import(
+	
+	"strings"
+	"strconv"
+)
+func convertN(s string) string {
+	words := strings.Fields(s)
+	for i := 0; i < len(words); i++ {
+		if strings.HasPrefix(words[i],"(") && !strings.HasSuffix(words[i],")") {
+			a := words[i] + words[i+1]
+			a = strings.Trim(a,"()")
+			b := strings.Split(a,",")
+			c, _ := strconv.Atoi(b[1])
+				for j := i -c; j < i; j++{
+					switch b[0] {
+					case "up":
+						words[j] = strings.ToUpper(words[j])
+					case "low":
+						words[j] = strings.ToLower(words[j])
+					case"cap":
+						words[j] = strings.Title(words[j])
+					}
+				}
+				words = append(words[:i],words[i+2:]...)
+			} 
+		}
+		return strings.Join(words," ")
+	}
+	
